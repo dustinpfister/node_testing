849a5a5d0a26daaa985490de772f53f9b355fe73
dustinpfister <dustin.pfister@gmail.com>
2017-04-11 13:56:04 -0400

    listing file names

--- jimp/resize_collection_all.js
+++ jimp/resize_collection_all.js
@@ -1,27 +1,48 @@
 var Jimp = require('jimp'),
 fs = require('fs');
 
-// open a file called "test.png"
-Jimp.read('./source/blues.jpg', function (err, img) {
-
-    var sizes = [64, 128, 256],
-    quality = 10;
+fs.readdir('./source', function (err, data) {
 
     if (err) {
 
-        throw err;
+        console.log('Error');
+        console.log(err);
+
+    } else {
+
+        data.forEach(function (fileName) {
+
+            console.log(fileName);
+
+        });
 
     }
 
-    // resize for all sizes
-    sizes.forEach(function (size) {
+});
 
-        // resize, and save
-        //img.resize(size, size) // resize
-        img.scaleToFit(size, Jimp.AUTO, Jimp.RESIZE_BEZIER)
-        .quality(quality)
-        .write('./build/blues_stf_BEZIER_q' + quality + '_' + size + '.jpg'); // save
+/*
+// open a file called "test.png"
+Jimp.read('./source/blues.jpg', function (err, img) {
 
-    });
+var sizes = [64, 128, 256],
+quality = 10;
+
+if (err) {
+
+throw err;
+
+}
+
+// resize for all sizes
+sizes.forEach(function (size) {
+
+// resize, and save
+//img.resize(size, size) // resize
+img.scaleToFit(size, Jimp.AUTO, Jimp.RESIZE_BEZIER)
+.quality(quality)
+.write('./build/blues_stf_BEZIER_q' + quality + '_' + size + '.jpg'); // save
 
 });
+
+});
+*/
