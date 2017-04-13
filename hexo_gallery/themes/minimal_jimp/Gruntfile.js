75c01bee043fb2986dd24e20aee9f9d279d7fb2a
dustinpfister <dustin.pfister@gmail.com>
2017-04-13 11:15:56 -0400

    grunt file lists img folder

--- hexo_gallery/themes/minimal_jimp/Gruntfile.js
+++ hexo_gallery/themes/minimal_jimp/Gruntfile.js
@@ -1,10 +1,25 @@
 module.exports = function (grunt) {
 
+    var fs = require('fs'),
+    sourcePath = '../../source/img';
+
     // Default task(s).
     grunt.registerTask('default', function () {
 
         console.log('minimal_jimp default grunt task:');
         console.log('nothing yet.');
+
+        done = this.async();
+
+        fs.readdir(sourcePath, function (one, two) {
+
+            console.log('read dir:');
+            console.log(one);
+            console.log(two);
+
+            done();
+
+        });
 
     });
 
