3de21ff504ad65cfab1821dc3ba234be72f8b479
dustinpfister <dustin.pfister@gmail.com>
2017-04-13 11:09:34 -0400

    gruntfile

---
+++ hexo_gallery/themes/minimal_jimp/Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function (grunt) {
+
+    // Default task(s).
+    grunt.registerTask('default', function () {
+
+        console.log('minimal_jimp default grunt task:');
+        console.log('nothing yet.');
+
+    });
+
+};
