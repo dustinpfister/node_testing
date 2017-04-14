d6d5feb1e0f6f1b5ec7b322526b31ab020296469
dustinpfister <dustin.pfister@gmail.com>
2017-04-14 13:29:44 -0400

    cli, and hexo gallery update

--- cli_hello_world/command.js
+++ cli_hello_world/command.js
@@ -1,1 +1,10 @@
-console.log('I am a cli tool');
+
+var args = process.argv.slice(2, process.argv.length);
+
+console.log('I am a simple addation cli tool');
+
+if (args.length >= 2) {
+
+    console.log(Number(args[0]) + Number(args[1]));
+
+}
--- hexo_gallery/source/gallery/index.md
+++ hexo_gallery/source/gallery/index.md
@@ -2,7 +2,3 @@
 title: gallery index
 photos:
 ---
-
-![blues](/img/blues.jpg)
-![blues](/img/beyonce-by-Isa.jpg)
-![blues](/img/barack-and-flag.jpg)
