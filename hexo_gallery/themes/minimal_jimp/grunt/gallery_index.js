3dd0ff3327f30b32aa3b36b8157506f54cc18adf
dustinpfister <dustin.pfister@gmail.com>
2017-04-15 17:05:36 -0400

    looping filenames

--- hexo_gallery/themes/minimal_jimp/grunt/gallery_index.js
+++ hexo_gallery/themes/minimal_jimp/grunt/gallery_index.js
@@ -1,8 +1,22 @@
+var fs = require('fs'),
+markdownPath = '../../source/gallery';
 
 exports.runScript = function (done) {
 
     console.log('building index.');
 
-    done();
+    fs.readdir(markdownPath, function (err, files) {
+
+        //console.log(files);
+
+        files.forEach(function (filename) {
+
+            console.log(filename);
+
+        });
+
+        done();
+
+    });
 
 };
