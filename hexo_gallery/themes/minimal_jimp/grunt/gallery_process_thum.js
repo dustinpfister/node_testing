9c4a0545a56f7e20993bf307150269325000cad0
dustinpfister <dustin.pfister@gmail.com>
2017-04-16 10:06:38 -0400

    making collection thum image

--- hexo_gallery/themes/minimal_jimp/grunt/gallery_process_thum.js
+++ hexo_gallery/themes/minimal_jimp/grunt/gallery_process_thum.js
@@ -8,6 +8,9 @@ dir = require('node-dir');
 // process thum images
 exports.runScript = function (done) {
 
+    var index = 0,
+    currentColl = '';
+
     dir.readFiles(
 
         // path to read
@@ -25,15 +28,45 @@ exports.runScript = function (done) {
 
         Jimp.read(filename, function (err, img) {
 
-            console.log('reading file: ' + filename);
+            var ns = filename.split('\\'),
+            collName = ns[ns.length - 2];
 
+            console.log('reading file # ' + index + ' : ' + filename);
+            console.log('collection name: ' + collName);
+
+            // make a thum for the collection
+            if (currentColl != collName) {
+
+                currentColl = collName;
+                console.log('Collection Change, making thum:');
+                //console.log(ns.shift().join('//'));
+
+                img.scaleToFit(64, Jimp.AUTO, Jimp.RESIZE_BEZIER)
+                .quality(30)
+                .write(
+                    ns.slice(0, ns.length - 2).join('//') + '/' + collName + '/thum.jpg',
+                    function () {
+
+                    console.log('made a thum image for collection : ' + collName);
+
+                });
+
+            }
+            console.log('');
+
+            /*
             img.scaleToFit(64, Jimp.AUTO, Jimp.RESIZE_BEZIER)
             .quality(30)
             .write(filename.replace(/\.jpg$|\.png$/, '') + '_jimped_64.jpg', function () {
 
-                next();
+            next();
 
             });
+             */
+
+            index += 1;
+
+            next();
 
         });
 
