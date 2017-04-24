0c786570e9574f315da7189d5673c716b36aa071
dustinpfister <dustin.pfister@gmail.com>
2017-04-24 08:15:40 -0400

    new get collection names method

--- hexo_gallery/themes/minimal_jimp/grunt/gallery_thums.js
+++ hexo_gallery/themes/minimal_jimp/grunt/gallery_thums.js
@@ -1,11 +1,58 @@
 // process gallery thum images
 
 var Jimp = require('jimp'),
+fs = require('fs'),
 galleryPath = '../../source/img/gallery_collections',
-dir = require('node-dir');
+dir = require('node-dir'),
+path = require('path');
+
+/*
+function getDirectories (srcpath) {
+return fs.readdirSync(srcpath)
+.filter(file => fs.statSync(path.join(srcpath, file)).isDirectory())
+}
+ */
+
+getCollectionFolders = function () {
+
+    fs.readdir(galleryPath, function (err, contents) {
+
+        console.log(contents);
+
+        var f = contents.filter(function (file) {
+
+                var filePath = path.join(galleryPath, file);
+
+                fs.stat(filePath, function (err, data) {
+
+                    console.log(err);
+                    console.log(data)
+
+                });
+
+            });
+
+        console.log(f);
+
+    });
+
+};
+
+/*
+dir.readFiles(galleryPath, {}, function (err, content, filename, next) {
+
+console.log(err);
+console.log(filename);
+
+next();
+
+}, function () {});
+ */
 
 exports.runScript = function (options, done) {
 
     console.log('gallery_thums.');
+
+    getCollectionFolders();
 
 };
