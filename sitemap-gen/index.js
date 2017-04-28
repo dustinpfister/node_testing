4f69429c884c6cbdcfbaf1055dc28301db703561
dustinpfister <dustin.pfister@gmail.com>
2017-04-28 18:58:20 -0400

    crawling my site

--- sitemap-gen/index.js
+++ sitemap-gen/index.js
@@ -1,12 +1,22 @@
 var SitemapGenerator = require('sitemap-generator');
  
 // create generator
-var generator = new SitemapGenerator('http://example.com');
+var generator = new SitemapGenerator('http://dustinpfister.github.io');
  
 // register event listeners
 generator.on('done', function (sitemaps) {
-  console.log(sitemaps); // => array of generated sitemaps
+
+  console.log(sitemaps[0]); // => array of generated sitemaps
+
 });
+
+generator.on('fetch', function (status, url) {
+  // log url
+  
+  console.log('fetch: ' + url);
+  
+});
+
  
 // start the crawler
 generator.start();
