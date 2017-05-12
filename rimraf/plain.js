72705c1002439e9e87fa5daf33060d77316754a5
dustinpfister <dustin.pfister@gmail.com>
2017-05-12 18:02:25 -0400

    plain example update

--- rimraf/plain.js
+++ rimraf/plain.js
@@ -1,16 +1,41 @@
 var fs = require('fs'),
 path = require('path');
 
-var readDir = function (dir) {
+var readDir = function (dir, forItem) {
 
-    fs.readdir(dir, function (one, two) {
+    forItem = forItem || function (itemLoc) {
+        console.log(itemLoc)
+    };
 
-        console.log(two[1]);
+    // read the given dir
+    fs.readdir(dir, function (err, content) {
 
-        console.log(fs.lstatSync(path.join(dir, '/' + two[1])).isDirectory());
+        // for all contents in the path
+        content.forEach(function (item) {
+
+            itemLoc = path.join(dir, '/' + item);
+
+            // log the current item
+            forItem(itemLoc);
+
+            // if a dir, continue recursively
+            if (fs.lstatSync(itemLoc).isDirectory()) {
+
+                readDir(itemLoc);
+
+            }
+
+        });
 
     });
 
 };
 
-readDir('./source');
+readDir('./source', function (itemLoc) {
+
+    //console.log(itemLoc);
+
+    itemLoc.match(/txt$/);
+
+
+});
