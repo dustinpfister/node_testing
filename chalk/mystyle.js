a1a179f5364127af0e8cc089d8e87cc07f25cf76
dustinpfister <dustin.pfister@gmail.com>
2017-05-19 16:33:14 -0400

    new project

---
+++ chalk/mystyle.js
@@ -0,0 +1,11 @@
+var chalk = require('chalk'),
+
+myStyle = chalk.bold.green.dim;
+sad = chalk.blue.dim
+coolText = chalk.blue;
+
+
+console.log(myStyle('This is my term text style'));
+console.log(sad('no buddy loves me.'));
+console.log(coolText('cool bro'));
+//console.log(chalk);
