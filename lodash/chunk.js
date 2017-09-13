d9debf5f975f6a97950a949c6b73778d113cabf5
dustinpfister <dustin.pfister@gmail.com>
2017-09-13 15:05:21 -0400

    chunk demo looking good

--- lodash/chunk.js
+++ lodash/chunk.js
@@ -1,20 +1,33 @@
-var _ = require('lodash');
+var _ = require('lodash'),
+chalk = require('chalk');
 
+// basic example
 var arr = ['one', 'two', 'three', 'four', 'five', 'six'];
 console.log(_.chunk(arr, 2));
 
+
+// matrix data example
 var data = {
 
     w : 4,
-    colors : ['black', 'white'],
+    colors : ['white', 'blue'],
     px : [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1]
 
 },
-
 matrix = _.chunk(data.px, data.w);
 
+// draw it out like this
 matrix.forEach(function (line) {
 
-    console.log(line);
+    var txt = '';
+    line.forEach(function (px) {
+
+        var style = chalk[data.colors[px]];
+
+        txt += style(px);
+
+    });
+
+    console.log(txt);
 
 });
