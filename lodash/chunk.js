71aefe63b5270626ea52523f7779f592fe5da7c6
dustinpfister <dustin.pfister@gmail.com>
2017-09-13 15:23:25 -0400

    demo update

--- lodash/chunk.js
+++ lodash/chunk.js
@@ -3,21 +3,28 @@ chalk = require('chalk');
 
 // basic example
 var arr = ['one', 'two', 'three', 'four', 'five', 'six'];
-console.log(_.chunk(arr, 2));
 
+console.log(_.chunk(arr, 2));
+// [ [ 'one', 'two' ], [ 'three', 'four' ], [ 'five', 'six' ] ]
 
 // matrix data example
 var data = {
-
+ 
     w : 4,
     colors : ['white', 'blue'],
-    px : [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1]
-
-},
-matrix = _.chunk(data.px, data.w);
+    px : [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+ 
+    // my toMatrix method using _.chunk
+    toMatrix : function(){
+ 
+        return _.chunk(this.px,this.w);
+ 
+    }
+ 
+};
 
 // draw it out like this
-matrix.forEach(function (line) {
+data.toMatrix().forEach(function (line) {
 
     var txt = '';
     line.forEach(function (px) {
