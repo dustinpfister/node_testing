72af326afa671f67ffa7b8e438d2abbc287553c1
dustinpfister <dustin.pfister@gmail.com>
2018-09-30 14:36:43 -0400

    chalk demo update

--- chalk/red-text-demo.js
+++ chalk/red-text-demo.js
@@ -4,3 +4,7 @@ var chalk = require('chalk');
 
 console.log(chalk.red);
 console.log('normal \u001b[31m red \u001b[39m normal');
+
+// node -e "console.log('normal \u001b[31m red \u001b[39m normal');"
+
+
